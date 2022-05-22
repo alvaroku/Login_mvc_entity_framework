f706ffe0c7316c4ff2021d215e7469f3e6a96e39
alvaroku <dual.alvaro.ku@gmail.com>
2022-05-22 02:04:32 -0500

    generando el proyecto

---
+++ Login_mvc_entity_framework/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Login_mvc_entity_framework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
